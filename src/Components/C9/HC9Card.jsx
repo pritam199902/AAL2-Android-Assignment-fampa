78492ec07114617cb8b841f73ff14c8e6079fa6f
Pritam <www.pritamdas1999@gmail.com>
2021-07-26 20:58:38 +0530

    upload full setup 03

--- src/Components/C9/HC9Card.jsx
+++ src/Components/C9/HC9Card.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './style.css'
 
 function HC9Card({ data, maxheight }) {
-    // console.log(maxheight);
     return (
         <a href={data.url} style={{ height: '100%' }} >
             <img src={data.bg_image.image_url} alt="" className="mx-1" style={{aspectRatio: data.aspect_ratio, height: maxheight, borderRadius: '10px'}} />
